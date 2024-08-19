60b46d61867bb845b80a42c4f108403dac877311
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2024-08-20 03:39:59 +0800

    添加一个与黑神话悟空预售有关的小脚本
    toy/ 文件夹以后就有用啦

---
+++ toy/black-monkey-time.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# 在 chatbox 里显示距离黑神话悟空还有多久结束预下载
+# 一个小玩具
+
+target_string="2024-08-20 10:00"
+
+while true; do
+    today=$(($(date +%s) / 60))
+    target=$(($(date --date "${target_string}" +%s) / 60))
+    minutes=$(($target - $today))
+    ./main.out "距离 ${target_string} 还有 ${minutes} 分钟"
+    sleep 10
+done
