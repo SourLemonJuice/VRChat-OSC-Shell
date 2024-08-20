cf192d4c3112d48888dddd3280df56d7752b0fb7
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2024-08-20 22:19:50 +0800

    更新脚本们中调用主程序的参数

--- toy/black-monkey-time.sh
+++ toy/black-monkey-time.sh
@@ -9,6 +9,6 @@ while true; do
     today=$(($(date +%s) / 60))
     target=$(($(date --date "${target_string}" +%s) / 60))
     minutes=$(($target - $today))
-    ./main.out "距离 ${target_string} 还有 ${minutes} 分钟" || exit 1
+    ./main.out chatbox "距离 ${target_string} 还有 ${minutes} 分钟" || exit 1
     sleep 10
 done
--- toy/current-time.sh
+++ toy/current-time.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 while true; do
-    ./main.out "$(date --iso-8601=seconds)" || exit 1
+    ./main.out chatbox "$(date --iso-8601=seconds)" || exit 1
     sleep 2
 done
