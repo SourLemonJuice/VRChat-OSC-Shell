2d32014a97f1c7f96b10e1717ee7fb13f64475bb
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2024-08-20 22:11:02 +0800

    更改命令行选项的结构，以及完善了参数的输入
    以后准备用上子命令啦。也为一些输入错误的场景做了正确的报错

--- source/main.c
+++ source/main.c
@@ -12,34 +12,75 @@
 
 enum ExitCode {
     kExitSuccess = EXIT_SUCCESS,
-    kExitStdError = EXIT_FAILURE,
+    kExitErrorStd = EXIT_FAILURE,
+    kExitErrorGetFlag,
+    kExitErrorSocket,
 };
 
-int main(int argc, char *argv[])
+void SubcommandChatbox(int argc, char *argv[], int arg_now)
 {
-    char *message;
-    if (argc >= 2) {
-        message = argv[1];
-    } else {
-        message = "Hello VRChat OSC!!!";
+    arg_now++;
+    if (argc - 1 > arg_now) {
+        printf("Chatbox only need one argument, but now has %d\n", argc - arg_now);
+        exit(kExitErrorGetFlag);
     }
 
+    char *message = "Hello VRChat OSC(Chatbox)";
+    if (arg_now < argc)
+        message = argv[arg_now];
+
+    // VRChat chatbox only allow message of less than 144 characters
+    int message_len = strlen(message);
+    if (message_len > 144) {
+        printf("VRChat limited chatbox characters to 144, but the input is %d\n", message_len);
+        exit(kExitErrorStd); // TODO add new error type
+    }
+
+    // VRChat OSC server socket
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (fd < 0) {
         perror("Socket create error");
-        exit(kExitStdError);
+        exit(kExitErrorStd);
     }
 
-    // connect to VRChat OSC server
     struct sockaddr_in des_addr = {0};
     des_addr.sin_family = AF_INET;
     des_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
     des_addr.sin_port = htons(9000);
 
-    char buffer[1024] = {0};
+    char buffer[256] = {0};
     int len = tosc_writeMessage(buffer, sizeof(buffer), "/chatbox/input", "sT", message);
 
-    sendto(fd, buffer, len, 0, (struct sockaddr *)&des_addr, sizeof(des_addr));
+    if (sendto(fd, buffer, len, 0, (struct sockaddr *)&des_addr, sizeof(des_addr)) == -1) {
+        perror("Send message error");
+        exit(kExitErrorSocket);
+    }
+    printf("Sended message: \"%s\"\n", message);
 
     close(fd);
+
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        printf("No action selected\n");
+        exit(kExitErrorStd);
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "help") == 0) {
+            printf("So, hey shell, print a help!\n");
+            exit(kExitSuccess);
+        } else if (strcmp(argv[i], "chatbox") == 0) {
+            SubcommandChatbox(argc, argv, i);
+            exit(kExitSuccess);
+        } else {
+            printf("Unknown subcommand \"%s\"\n", argv[i]);
+            exit(kExitErrorGetFlag);
+        }
+    }
+
+    return kExitSuccess;
 }
