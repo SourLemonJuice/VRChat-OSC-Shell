e9ef2e4ed2982975f3f0dd75fb6ef70dcd6e214a
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2024-08-22 15:20:10 +0800

    添加 typing 模式
    怎么越来越丑了
    
    其实还想用用其他命令行解析库来着的,但没接上

--- source/main.c
+++ source/main.c
@@ -61,6 +61,65 @@ void SubcommandChatbox(int argc, char *argv[], int arg_now)
 
     close(fd);
 
+    exit(kExitSuccess);
+    return;
+}
+
+void SubcommandTyping(int argc, char *argv[], int arg_now)
+{
+    enum {
+        kTypingOn,
+        kTypingOff,
+    } typing_status;
+
+    arg_now++;
+    if (arg_now + 1 != argc) {
+        printf("Too many or too few arguments\n");
+        exit(kExitErrorGetFlag);
+    }
+
+    if (strcmp(argv[arg_now], "on") == 0) {
+        typing_status = kTypingOn;
+    } else if (strcmp(argv[arg_now], "off") == 0) {
+        typing_status = kTypingOff;
+    } else {
+        printf("Unknown typing status: %s\n", argv[arg_now]);
+        exit(kExitErrorGetFlag);
+    }
+
+    // VRChat OSC server socket
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0) {
+        perror("Socket create error");
+        exit(kExitErrorStd);
+    }
+
+    struct sockaddr_in des_addr = {0};
+    des_addr.sin_family = AF_INET;
+    des_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    des_addr.sin_port = htons(9000);
+
+    char buffer[256] = {0};
+    char *format;
+    switch (typing_status) {
+    case kTypingOn:
+        format = "T";
+        break;
+    case kTypingOff:
+        format = "F";
+        break;
+    }
+    int len = tosc_writeMessage(buffer, sizeof(buffer), "/chatbox/typing", format);
+
+    if (sendto(fd, buffer, len, 0, (struct sockaddr *)&des_addr, sizeof(des_addr)) == -1) {
+        perror("Send message error");
+        exit(kExitErrorSocket);
+    }
+    printf("Sended typing status with format: %s\n", format);
+
+    close(fd);
+
+    exit(kExitSuccess);
     return;
 }
 
@@ -75,12 +134,14 @@ int main(int argc, char *argv[])
         if (strcmp(argv[i], "help") == 0 or strcmp(argv[i], "--help") == 0 or strcmp(argv[i], "-h") == 0) {
             printf("Usage: execName <MODE> [--help | -h]\n\n");
             printf("MODE:\n");
-            printf("\thelp\t\tShow help info, --help also will do this\n");
-            printf("\tchatbox\t\tSend chatbox message\n");
+            printf("\thelp\t\t\tShow help info, --help also will do this\n");
+            printf("\tchatbox <string>\tSend chatbox message\n");
+            printf("\ttyping <on/off>\t\tControl the typing indicator on or off\n");
             exit(kExitSuccess);
         } else if (strcmp(argv[i], "chatbox") == 0) {
             SubcommandChatbox(argc, argv, i);
-            exit(kExitSuccess);
+        } else if (strcmp(argv[i], "typing") == 0) {
+            SubcommandTyping(argc, argv, i);
         } else {
             printf("Unknown subcommand \"%s\"\n", argv[i]);
             exit(kExitErrorGetFlag);
